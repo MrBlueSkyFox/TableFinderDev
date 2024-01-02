301ffe6aad2fecaf178f1bf31b4b62e0bbb0c5d8
tigra <tigranium123@gmail.com>
2024-01-02 19:50:42 +0300

    move to separate dir

--- service_layer/table_model_interface.py
+++ service_layer/table_model_interface.py
@@ -1,14 +1,14 @@
 from abc import abstractmethod
 
 from transformers import AutoImageProcessor, AutoModelForObjectDetection
-
-from .types import probability
+import PIL.Image
+from . import types
 
 default_transformer_cache = r"models"
 
 
 class TableInterface:
-    def __init__(self, path_to_model: str, model_name: str, detection_threshold: probability):
+    def __init__(self, path_to_model: str, model_name: str, detection_threshold: types.probability):
         if len(path_to_model) < 0:
             path = default_transformer_cache
         else:
@@ -21,5 +21,5 @@ class TableInterface:
         self.model: AutoModelForObjectDetection
 
     @abstractmethod
-    def use_detection(self, image):
+    def use_detection(self, image: PIL.Image.Image):
         pass
