3613a814a4400a27a8d9a41c42fa4d5be25e8750
tigra <tigranium123@gmail.com>
2024-01-02 22:30:29 +0300

    made function more custom

--- util.py
+++ util.py
@@ -11,10 +11,28 @@ def get_all_jpg_images_in_directory(directory):
         A list of all JPEG images in the given directory.
     """
 
-    jpg_images = []
+    jpg_images = get_all_with_ext_in_dir(directory, ".jpg")
+    return jpg_images
+
+
+def get_all_pdf_files_in_directory(directory: str):
+    pdf_files = get_all_with_ext_in_dir(directory, ".pdf")
+    return pdf_files
+
+
+def get_all_with_ext_in_dir(directory: str, ext: str):
+    ext_files = []
     for filename in os.listdir(directory):
-        if filename.endswith(".jpg"):
-            jpg_images.append(os.path.join(directory, filename))
+        if filename.endswith(ext):
+            ext_files.append(os.path.join(directory, filename))
+
+    return ext_files
+
+
+def get_basename(path: str) -> str:
+    base_file_name = os.path.basename(path)
+    base_file_name = os.path.splitext(base_file_name)[0]
+    return base_file_name
 
 
 def create_output_json(path: str, file_name: str) -> str:
